69732ab64093efa9ad2bc5c1ca805ef45a110dc7
firedesse <165275437+firedesse@users.noreply.github.com>
2024-04-08 13:00:23 +0200

    Add files via upload

--- SwitchGame.py
+++ SwitchGame.py
@@ -19,7 +19,7 @@ FONT = pygame.font.Font(None, FONT_SIZE)
 
 # Create the game window
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Switch")
+pygame.display.set_caption("SwitchGame")
 
 def generate_question():
         num1 = random.randint(1, 20)
@@ -41,25 +41,25 @@ def draw_text(text, font, color, surface, x, y):
     surface.blit(textobj, textrect)
 
 def Calcul_mental():
-    global question,answer,score_calcul
-    clock = pygame.time.Clock()
-    lives = 3
+    global question,answer,score_calcul, score_switch, nb_switch
     time_per_question = 10  # Temps en secondes par question
     current_time = pygame.time.get_ticks()
     user_answer = "" # Variable pour stocker la réponse de l'utilisateur
     game_over = False
     start_time = time.time()
-    game_duration = 5
+    game_duration = switch_time
+    pygame.display.flip()
 
     while not game_over:
         screen.fill(WHITE)
         if time.time() - start_time >= game_duration:
+            screen.fill(BLACK)
+            pygame.display.flip()
             return
 
         # Gestion du temps
         time_left = time_per_question - (pygame.time.get_ticks() - current_time) // 1000
         if time_left <= 0:
-            lives -= 1
             current_time = pygame.time.get_ticks()
         else:
             # Générer une question si le temps n'est pas écoulé
@@ -71,9 +71,9 @@ def Calcul_mental():
             screen.blit(question_surface, question_rect)
 
             # Afficher le score, le nombre de vies et le temps restant
-            draw_text(f"Score: {score_calcul}", FONT, BLACK, screen, 10, 10)
-            draw_text(f"Temps restant: {time_left} s", FONT, BLACK, screen, WIDTH - 310, 10)
-                
+            draw_text(f"Score: {score_switch}", FONT, BLACK, screen, 10, 10)
+            draw_text(f"{time_left} s", FONT, BLACK, screen, WIDTH - 420, 10)
+            draw_text(f"Switch: {nb_switch}", FONT, BLACK, screen, WIDTH - 160 , 10)
             # Afficher la réponse de l'utilisateur
             draw_text(f"Réponse: {user_answer}", FONT, BLACK, screen, WIDTH//2 - 100, HEIGHT - 100)
 
@@ -92,7 +92,13 @@ def Calcul_mental():
                             score_calcul += 1
                             question=""
                             answer=""
+                            if score_calcul == 3:
+                                score_switch += 1
+                                nb_switch += 1
+                                score_calcul=0
                         else:
+                            screen.fill(BLACK)
+                            pygame.display.flip()
                             return
                         current_time = pygame.time.get_ticks()
                         user_answer = "" # Effacer la réponse de l'utilisateur après vérification
@@ -101,9 +107,6 @@ def Calcul_mental():
                 else:
                     if event.unicode.isdigit() or (event.unicode == '-' and len(user_answer) == 0): # Autoriser uniquement les chiffres et le signe négatif
                         user_answer += event.unicode
-
-        # Limiter la vitesse de la boucle de jeu
-        clock.tick(30)
 
 # Fonction pour dessiner le joueur
 def draw_player(x, y):
@@ -149,7 +152,7 @@ def point_inside_circle(x, y):
 
 # Fonction pour le Jeu_Des_Fleches
 def Jeu_Des_Fleches():
-    global player_x, player_y, score, arrow_delay, ARROW_WAVE_DELAY, game_over, arrow_count, arrows, CIRCLE_CENTER, CIRCLE_RADIUS, PLAYER_RADIUS, ARROW_SIZE
+    global player_x, player_y, score_fleche, arrow_delay, ARROW_WAVE_DELAY, game_over, arrow_count, arrows, CIRCLE_CENTER, CIRCLE_RADIUS, PLAYER_RADIUS, ARROW_SIZE, score_switch, nb_switch
 
     PLAYER_RADIUS = 10
     PLAYER_SPEED = 5
@@ -163,7 +166,6 @@ def Jeu_Des_Fleches():
     CIRCLE_RADIUS = 290
     CIRCLE_CENTER = (WIDTH // 2, HEIGHT // 2)
 
-    score = 0
     arrow_delay = INITIAL_ARROW_DELAY
     ARROW_WAVE_DELAY = 5  # Initialisation de ARROW_WAVE_DELAY
     ARROW_WAVE_DELAY_REDUCTION = 1
@@ -172,12 +174,15 @@ def Jeu_Des_Fleches():
     game_duration = switch_time
 
     clock = pygame.time.Clock()
+    pygame.display.flip()
     running = True
 
     # Boucle principale du jeu
     while running:
         screen.fill(WHITE)
         if time.time() - start_time >= game_duration:
+            screen.fill(BLACK)
+            pygame.display.flip()
             return
 
         # Gestion des événements
@@ -193,6 +198,8 @@ def Jeu_Des_Fleches():
             player_x = WIDTH // 2
             player_y = HEIGHT // 2
             arrow_count = 0
+            screen.fill(BLACK)
+            pygame.display.flip()
             return
 
         # Déplacement du joueur
@@ -246,10 +253,14 @@ def Jeu_Des_Fleches():
         arrows = [arrow for arrow in arrows if 0 <= arrow['x'] <= WIDTH and 0 <= arrow['y'] <= HEIGHT]
 
         # Affichage du score
-        score += 1
-        font = pygame.font.SysFont(None, 30)
-        text = font.render("Score: " + str(score), True, BLACK)
-        screen.blit(text, (10, 10))
+        score_fleche += 1
+        if score_fleche >= 400:
+            score_switch += 1
+            nb_switch += 1
+            score_fleche = 0
+
+        draw_text(f"Score: {score_switch}", FONT, BLACK, screen, 10, 10)
+        draw_text(f"Switch: {nb_switch}", FONT, BLACK, screen, WIDTH - 160 , 10)
 
         # Mettre à jour l'affichage
         pygame.display.flip()
@@ -259,12 +270,15 @@ def Jeu_Des_Fleches():
 
 # Fonction pour le Memory
 def Memory():
-    global selected_tiles,matched_tiles,numbers
+    global selected_tiles, matched_tiles, numbers, score_switch, nb_switch, score_memory
     # Define tile properties
     TILE_SIZE = 100
     GAP_SIZE = 10
     ROWS = 4
     COLS = 4
+    draw_text(f"Score: {score_switch}", FONT, BLACK, screen, 10, 10)
+    draw_text(f"Switch: {nb_switch}", FONT, BLACK, screen, WIDTH - 160 , 10)
+    pygame.display.flip()
 
     # Create numbers for tiles
     if numbers==[]:
@@ -282,13 +296,17 @@ def Memory():
     # Game variables
     turns = 0
     start_time = time.time()
-    game_duration = 5
+    game_duration = switch_time
 
     # Main game loop
     running = True
     while running:
         screen.fill(WHITE)
+        draw_text(f"Score: {score_switch}", FONT, BLACK, screen, 10, 10)
+        draw_text(f"Switch: {nb_switch}", FONT, BLACK, screen, WIDTH - 160, 10)
         if time.time() - start_time >= game_duration:
+            screen.fill(BLACK)
+            pygame.display.flip()
             running = False
 
         for event in pygame.event.get():
@@ -319,16 +337,26 @@ def Memory():
             index1, index2 = selected_tiles
             if numbers[index1] == numbers[index2]:
                 matched_tiles.extend(selected_tiles)
+                score_memory+=1
+                if score_memory==2:
+                    nb_switch += 1
+                    score_switch += 1
+                    score_memory = 0
                 selected_tiles = []
             else:
                 time.sleep(1)
                 selected_tiles = []
 
-            turns += 1
-
         # Check for game over
         if len(matched_tiles) == len(numbers):
+            nb_switch += 1
+            selected_tiles = []
+            matched_tiles = []
+            numbers = []
+            screen.fill(BLACK)
+            pygame.display.flip()
             return
+    pygame.display.flip()
 
 # Fonction pour le Snake
 def our_snake(snake_block, snake_list):
@@ -337,7 +365,7 @@ def our_snake(snake_block, snake_list):
 
 def message(msg, color):
     mesg = FONT.render(msg, True, color)
-    text_rect = mesg.get_rect(center=(WIDTH // 2, HEIGHT // 2))
+    text_rect = mesg.get_rect(center=(WIDTH // 2, (HEIGHT // 2)-10))
     screen.blit(mesg, text_rect)
 
 def handle_events():
@@ -347,21 +375,23 @@ def handle_events():
             sys.exit()
 
 def Snake():
-    global Length_of_snake, x1, y1, x1_change, y1_change, snake_List, Length_of_snake, score_snake, foodx, foody
+    global Length_of_snake, x1, y1, x1_change, y1_change, snake_List, Length_of_snake, score_snake, foodx, foody, nb_switch, score_switch
     start_time = time.time()
     game_duration = switch_time
     snake_block = 10
-    snake_speed = 15
+    snake_speed = 20
     
     clock = pygame.time.Clock()
+    pygame.display.flip()
 
     game_over = False
-
     while not game_over:
         game_close = False
 
         while not game_close:
             if time.time() - start_time >= game_duration:
+                screen.fill(BLACK)
+                pygame.display.flip()
                 return
 
             handle_events()
@@ -381,7 +411,18 @@ def Snake():
                 x1_change = 0
 
             if x1 >= WIDTH - 60 or x1 < 60 or y1 >= HEIGHT - 60 or y1 < 60:
-                game_close = True
+                x1 = WIDTH / 2
+                y1 = HEIGHT / 2
+                x1_change = 0
+                y1_change = 0
+                snake_List = []
+                Length_of_snake = 1
+                score_snake = 0
+                foodx = round(random.randrange(61, WIDTH - 61) / 10.0) * 10.0
+                foody = round(random.randrange(61, HEIGHT - 61) / 10.0) * 10.0
+                screen.fill(BLACK)
+                pygame.display.flip()
+                return
 
             x1 += x1_change
             y1 += y1_change
@@ -411,10 +452,13 @@ def Snake():
                 foody = round(random.randrange(61, HEIGHT - 61) / 10.0) * 10.0
                 Length_of_snake += 1
                 score_snake += 1
+                if score_snake == 3:
+                    nb_switch += 1
+                    score_switch += 1
+                    score_snake = 0
 
-            font = pygame.font.Font(None, 36)
-            score_text = font.render("Score: " + str(score_snake), True, (255, 255, 255))
-            screen.blit(score_text, (10, 10))  # Afficher le score dans le coin supérieur gauche
+            draw_text(f"Score: {score_switch}", FONT, WHITE, screen, 10, 10)
+            draw_text(f"Switch: {nb_switch}", FONT, WHITE, screen, WIDTH - 160 , 10)
             pygame.display.update()
 
             clock.tick(snake_speed)
@@ -429,40 +473,49 @@ def Snake():
             snake_List = []
             Length_of_snake = 1
             score_snake = 0
-            foodx = round(random.randrange(60, WIDTH - 60) / 10.0) * 10.0
-            foody = round(random.randrange(60, HEIGHT - 60) / 10.0) * 10.0
+            foodx = round(random.randrange(61, WIDTH - 61) / 10.0) * 10.0
+            foody = round(random.randrange(61, HEIGHT - 61) / 10.0) * 10.0
+            screen.fill(BLACK)
+            pygame.display.flip()
             return
 
 # Liste des jeux
-jeux = [Jeu_Des_Fleches, Calcul_mental, Memory, Snake]
+jeux = [Calcul_mental, Jeu_Des_Fleches, Memory, Snake]
 
-global switch_time, nb_switch
+global switch_time, nb_switch, start_time, score_switch
 
+score_switch =0
 current_game = None
 next_game=True
 new_game=None
-start_time = 0
-switch_time = 3  # Time in seconds before switching to a new game
+start_time = 0 # Time in seconds before switching to a new game
+switch_time = random.randint(5,15)
 arrows = []
 player_x = WIDTH // 2
 player_y = HEIGHT // 2
 arrow_count = 0
 x1 = WIDTH / 2
 y1 = HEIGHT / 2
-x1_change = 0
+x1_change = 10
 y1_change = 0
 snake_List = []
 Length_of_snake = 1
 score_snake = 0
-foodx = round(random.randrange(60, WIDTH - 60) / 10.0) * 10.0
-foody = round(random.randrange(60, HEIGHT - 60) / 10.0) * 10.0
+score_calcul = 0
+score_fleche = 0
+score_memory = 0
+foodx = round(random.randrange(61, WIDTH - 61) / 10.0) * 10.0
+foody = round(random.randrange(61, HEIGHT - 61) / 10.0) * 10.0
 question=""
 answer=""
 score_calcul=0
-nb_switch=2
+nb_switch=3
 selected_tiles = []
 matched_tiles = []
 numbers = []
+games_over = False
+clock = pygame.time.Clock()
+clock.tick(60)
 
 # Main loop
 while True:
@@ -472,13 +525,34 @@ while True:
         while current_game==new_game:
             new_game=random.choice(jeux)
         current_game=new_game
+        txt="Next Game: "+ (str(new_game)).split()[1]
+        if "_" in txt:
+            txt=txt.replace("_", " ")
+        message(txt,WHITE)
+        pygame.display.flip()
+        pygame.time.wait(2000)
         current_game()  # Start the selected game
-        start_time = time.time()  # Record the start time of the game
+        nb_switch -= 1
         next_game=False
     # Check if it's time to switch to a new game based on elapsed time
     if time.time() - start_time >= switch_time:
-        nb_switch -= 1
         next_game=True  # Reset the current game
+    if nb_switch <= 0:
+        games_over=True
+    while games_over:
+        screen.fill(WHITE)
+        message("Game Over",BLACK)
+        draw_text(f"Score: {score_switch}", FONT, BLACK, screen, WIDTH - 465, HEIGHT - 290)
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                nb_switch = 0
+                game_over = False
+
+            
 
     # Handle events
     for event in pygame.event.get():
